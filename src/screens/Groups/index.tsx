7ccce60a11a2a73e441cf9d4cc01117684e4b582
biluadev <biluadevcod@gmail.com>
2025-03-31 20:09:12 +0100

    Header done!

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -1,11 +1,13 @@
 // import { Container, Title } from "./styles";
 import * as S from "./styles";
 import { Header } from "@components/Header";
+import { Highlight } from "@components/Highlight";
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
+      <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
     </S.Container>
   );
 }

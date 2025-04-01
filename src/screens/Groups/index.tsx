a2cfdb7c84f3286c6b036cd945ed2c76f6a78668
biluadev <biluadevcod@gmail.com>
2025-04-01 10:10:16 +0100

    GroupCard Done!

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -2,12 +2,14 @@
 import * as S from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
+import { GroupCard } from "@components/GroupCard";
 
 export function Groups() {
   return (
     <S.Container>
       <Header />
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
+      <GroupCard title="Galera do ignite" />
     </S.Container>
   );
 }

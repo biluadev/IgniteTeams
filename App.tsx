fb6abf30d285daa46af68732508df78e9831fd3a
biluadev <biluadevcod@gmail.com>
2025-04-01 18:42:14 +0100

    Create new turm added

--- App.tsx
+++ App.tsx
@@ -5,9 +5,9 @@ import {
   Roboto_700Bold,
 } from "@expo-google-fonts/roboto";
 import theme from "./src/theme";
-import { Groups } from "@screens/Groups";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
+import { NewGroup } from "@screens/NewGroup";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <Groups /> : <Loading />}
+      {fontsLoaded ? <NewGroup /> : <Loading />}
     </ThemeProvider>
   );
 }

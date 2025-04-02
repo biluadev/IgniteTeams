e741fa41c09f0c462faad58838257d715cbf4b28
biluadev <biluadevcod@gmail.com>
2025-04-02 17:22:21 +0100

    Added component players

--- App.tsx
+++ App.tsx
@@ -7,7 +7,7 @@ import {
 import theme from "./src/theme";
 import { Loading } from "@components/Loading";
 import { StatusBar } from "react-native";
-import { NewGroup } from "@screens/NewGroup";
+import { Players } from "@screens/Players";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -18,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      {fontsLoaded ? <NewGroup /> : <Loading />}
+      {fontsLoaded ? <Players /> : <Loading />}
     </ThemeProvider>
   );
 }
